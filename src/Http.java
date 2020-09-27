1a869ed67d3e7b061eacf664369af62ffdd31845
Gabriel Costa <gabrielcosta@digix.com.br>
2020-09-27 19:43:56 -0400

    add tratador de request para arquivo ou diretorio

--- src/Http.java
+++ src/Http.java
@@ -1,12 +1,17 @@
 package src;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Http {
+    public static final String URL = "url";
+    public static final String METHOD = "method";
+    public static final String PROTOCOL_VERSION = "protocolVersion";
     private final int WAITING = 0;
     private final int CONNECTED = 1;
+    private final String SERVER_PATH = System.getProperty("user.dir");
 
     private int currentState = WAITING;
 
@@ -36,15 +41,30 @@ public class Http {
                 break;
             case CONNECTED:
                 requestData = parseRequest(request);
+
+                handleRequest(requestData.get(URL));
+
                 response = "HTTP/1.1 200 Document follows \n" +
                         "Server:  FACOM-CD-2020/1.0 \n" +
                         "Content-Type: text/plain \n" +
-                        "Bye. \n";
+                        "Bye.";
                 currentState = WAITING;
                 break;
         }
 
         return response;
+    }
+
+    private void handleRequest(String url) {
+        url = SERVER_PATH + url;
+        File myDir = new File(url);
+        if (myDir.isDirectory()) {
+            File[] contents = myDir.listFiles();
+            Arrays.stream(contents)
+                    .forEach(file ->
+                            System.out.println(file.getName() + ": " + file.length()));
+        }
+        // else if is a file else 404
     }
 
     private Map<String, String> parseRequest(String request) {
@@ -53,9 +73,9 @@ public class Http {
         String[] inputElements = request.split("%n");
         String[] requestFirstLine = inputElements[0].split(" ");
 
-        requestData.put("method", requestFirstLine[0]);
-        requestData.put("document", requestFirstLine[1]);
-        requestData.put("httpVersion", requestFirstLine[2]);
+        requestData.put(METHOD, requestFirstLine[0]);
+        requestData.put(URL, requestFirstLine[1]);
+        requestData.put(PROTOCOL_VERSION, requestFirstLine[2]);
 
         return requestData;
     }
