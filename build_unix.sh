e74cf6cd16738d9960db73ff9ef82f4c08f4890f
Gabriel Costa <gabrielcosta@digix.com.br>
2020-09-28 10:53:42 -0400

    cria scripts de build

---
+++ build_unix.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+compile() {
+  javac -d out -cp ".:src" src/Server.java;
+}
+
+run() {
+  echo "Running...";
+  java -cp "out" src.Server 8080;
+}
+
+execute() {
+  mkdir out;
+  compile && run;
+}
+
+execute;
