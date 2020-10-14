de0c260835ead47777b27f783d9210b73f7dbf1c
Gab Costa <gabcostadeveloper@gmail.com>
2020-10-13 22:35:05 -0400

    implementa thread para conexoes cliente

---
+++ java/src/RequestProcessor.java
@@ -0,0 +1,45 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.Socket;
+
+public class RequestProcessor implements Runnable {
+    private final Socket clientSocket;
+    private final Integer portNumber;
+
+    public RequestProcessor(Socket clientSocket, Integer portNumber) {
+        this.clientSocket = clientSocket;
+        this.portNumber = portNumber;
+    }
+
+    @Override
+    public void run() {
+        try {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine, outputLine;
+            StringBuilder request = new StringBuilder();
+
+            Http http = new Http(portNumber);
+
+            while ((inputLine = in.readLine()) != null) {
+                request.append(inputLine).append("\r\n");
+                if (inputLine.isEmpty()) {
+                    outputLine = http.processRequest(request.toString());
+                    out.println(outputLine);
+                    break;
+                }
+            }
+            out.close();
+            in.close();
+            clientSocket.close();
+        }catch(IOException exception) {
+            System.err.println(exception.getMessage());
+        }
+    }
+}
--- java/src/Server.java
+++ java/src/Server.java
@@ -1,33 +1,17 @@
 package src;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         int portNumber = Integer.parseInt(args[0]);
-
         ServerSocket serverSocket = startServer(portNumber);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        String inputLine, outputLine;
-        StringBuilder request = new StringBuilder();
-
-        Http http = new Http(portNumber);
-
-        while ((inputLine = in.readLine()) != null) {
-            request.append(inputLine).append("\r\n");
-            if (inputLine.isEmpty()) {
-                outputLine = http.processRequest(request.toString());
-                out.println(outputLine);
-                break;
-            }
+        while(true) {
+            Socket clientSocket = serverSocket.accept();
+            Thread t = new Thread(new RequestProcessor(clientSocket, portNumber));
+            t.start();
         }
     }
 
