327cca7866d5f47742fb0652427c5e1f10c6d848
mateusragazzi <mateusragazzi.b@gmail.com>
2020-10-25 17:36:26 -0400

    finalizado comentários

--- java/src/domain/response/Response.java
+++ java/src/domain/response/Response.java
@@ -3,12 +3,23 @@ package src.domain.response;
 import src.domain.HttpStatus;
 
 public abstract class Response {
+    /**
+     * Funções a serem sobrescritas dentro das classes que estendem Response.
+     */
     protected abstract String makeResponseBody();
 
     protected abstract String makeHeaders();
 
     public abstract String buildResponse();
 
+    /**
+     * Função invocada pelos métodos makeHeaders.
+     * Devolve o header formatado com status e o tamanho do arquivo.
+     *
+     * @param httpStatus indica o status HTTP.
+     * @param contentLength indica o tamanho do body a ser devolvido.
+     * @return header formatado e preenchido.
+     */
     protected String makeBaseHeaders(HttpStatus httpStatus, int contentLength) {
         return String.format("HTTP/1.1 " + httpStatus.getStatusCode() + " " + httpStatus.getMessage() + " " + "\r\n" +
                 "Server:  FACOM-CD-2020/1.0 \r\n" +
--- java/src/domain/response/ResponseFactory.java
+++ java/src/domain/response/ResponseFactory.java
@@ -8,6 +8,15 @@ public class ResponseFactory {
     private static String callStack = "";
     private static final String FILES_PATH = System.getProperty("user.dir");
 
+    /**
+     * Função que determina qual tipo de resposta será devolvida ao cliente.
+     * Ela invoca a classe responsável por responder aquela requisição.
+     * Todas elas estendem a classe Response, unificando assim o tipo de resposta.
+     *
+     * @param filePath indica o caminho solicitado pelo cliente.
+     * @param baseUrl indica a URL base do servidor
+     * @return texto de resposta contendo o header e o body.
+     */
     public static Response create(String filePath, URL baseUrl) {
         File requestedFile = new File(getFilePath(filePath));
 
@@ -23,6 +32,13 @@ public class ResponseFactory {
         return new NotFoundResponse();
     }
 
+    /**
+     * Função que retorna o caminho absoluto do arquivo a ser processado.
+     * Importante para o CGI-BIN e atualização da callStack.
+     *
+     * @param filePath caminho atual
+     * @return caminho absoluto do arquivo/pasta solicitado.
+     */
     private static String getFilePath(String filePath) {
         if (!filePath.isEmpty() && !filePath.equals("/favicon.icon") && !filePath.equals("/")) {
             callStack = filePath.replaceFirst("/", "") + '/';
