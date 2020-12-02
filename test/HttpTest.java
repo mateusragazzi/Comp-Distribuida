6eae73a5d0166fb4e31b1e25486d1d9949895758
Gab Costa <gabcostadeveloper@gmail.com>
2020-12-01 23:29:47 -0400

    adiciona suporte a testes

---
+++ test/HttpTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import src.Http;
+import src.adapter.rest.Request;
+import src.domain.exceptions.MethodNotAllowedException;
+
+import java.net.MalformedURLException;
+
+public class HttpTest {
+    private final Http HTTP = new Http(8080);
+
+    public HttpTest() throws MalformedURLException {
+    }
+
+    @Test
+    public void shouldParseARequestWithHttpBody() {
+        String requestData = "{ movie: { id: 1 } }";
+        Request httpRequest = new HttpRequestBuilder().withMethod("POST").withBody(requestData).build();
+
+        String response = HTTP.processRequest(httpRequest.toString());
+
+        Assertions.assertFalse(response.isEmpty());
+    }
+}
