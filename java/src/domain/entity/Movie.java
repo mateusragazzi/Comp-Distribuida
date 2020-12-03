8bc46c6b3ec80f6b4c961228cd3fcdc742c94014
mateusragazzi <mateusragazzi.b@gmail.com>
2020-12-02 22:22:21 -0400

    Inicio das DAOs e Conexão com o Banco

--- java/src/domain/entity/Movie.java
+++ java/src/domain/entity/Movie.java
@@ -1,7 +1,24 @@
 package src.domain.entity;
 
 public class Movie {
+    private Long ID;
     private String title;
-    private int yearOfRelease;
-    private String director;
+    private String synopsis;
+
+    public Movie(String title, String synopsis) {
+        this.title = title;
+        this.synopsis = synopsis;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
 }
