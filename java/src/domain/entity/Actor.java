f087be74576c1915dd651024de7ff70528b674dd
mateusragazzi <mateusragazzi.b@gmail.com>
2020-12-03 20:21:39 -0400

    Atores iniciados

--- java/src/domain/entity/Actor.java
+++ java/src/domain/entity/Actor.java
@@ -1,9 +1,16 @@
 package src.domain.entity;
 
+import java.util.Objects;
+
 public class Actor {
     private Long ID;
     private final String name;
     private final String birthdate;
+
+    public Actor(Long ID, String name, String birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
 
     public Actor(String name, String birthdate) {
         this.name = name;
@@ -24,5 +31,19 @@ public class Actor {
 
     public void setID(Long ID) {
         this.ID = ID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Actor actor = (Actor) o;
+        return Objects.equals(name, actor.name) &&
+                Objects.equals(birthdate, actor.birthdate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, birthdate);
     }
 }
